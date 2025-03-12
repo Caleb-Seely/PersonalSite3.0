cbac1e7c673bea0bbf780e7ea7b0fd49039f0490
Clug-Seal <calebseely@gmail.com>
2025-03-12 11:24:23 -0700

    Cash Cash Cache

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,46 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-            {
-            protocol: 'https',
-            hostname: 'maps.googleapis.com',
-            },
-            {
-            protocol: 'https',
-            hostname: 'i.scdn.co',
-            },
-        ],
+  headers: async () => [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
         },
+      ],
+    },
+  ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'maps.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'mosaic.scdn.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image-cdn-fa.spotifycdn.com',
+        port: '',
+        pathname: '/**',
+      },
+      // Add any other Spotify image domains you might encounter
+    ],
+  }
 };
 
-module.exports = {
-   headers: async () => [
-     {
-       source: '/(.*)',
-       headers: [
-         {
-           key: 'Cache-Control',
-           value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
-         },
-       ],
-     },
-   ],
- }
-
 export default nextConfig;
-
